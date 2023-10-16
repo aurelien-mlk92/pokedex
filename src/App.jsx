3888e62bbc7e5441bc62f2c75c44bf2954fe6b74
aurelien.mlk92 <aurelien.geninsimonet@gmail.com>
2023-10-16 09:30:37 +0200

    state 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,62 @@
-import PokemonCard from './components/PokemonCard'
+import React, { useState } from 'react';
 
 function App() {
 
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handlePreviousClick = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+
   const pokemonList = [
     {
-      name : "bulbasaur",
-      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-      imgSrc :""    },
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
 
-  ];
-  
+  const currentPokemon = pokemonList[pokemonIndex];
 
+  return (
+    <div>
+      <h1>{currentPokemon.name}</h1>
+      <img src={currentPokemon.imgSrc} alt="" />
 
- return (
-     <>
-      <PokemonCard
-      pokemon = {pokemonList[1]}/>
-    </>
-  ) ;
+      {pokemonIndex > 0 && (
+        <button onClick={handlePreviousClick}>Précédent</button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={handleNextClick}>Suivant</button>
+      )}
+    </div>
+  );
 }
 
-export default App ;
+export default App;
